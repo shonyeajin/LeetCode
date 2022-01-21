36d3e6907c7e8ea32568fdc6617780e28d1c17ee
dorothy <55689863+shonyeajin@users.noreply.github.com>
2022-01-21 11:03:41 +0900

    Create pow.py

---
+++ pow.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x**n
+
+
+c = Solution()
+print(c.myPow(2.00000, 10))
